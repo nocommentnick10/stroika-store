fa1d926700c4133e8edb4f63dc6ab57503993c84
gaymarrige <whodes.nick@gmail.com>
2023-01-14 17:34:07 +0900

    Add AllBrands page

---
+++ src/js/brands.js
@@ -0,0 +1,15 @@
+// MixItUp
+
+$('document').ready(function(){
+
+    let containerEl = document.querySelector('#mix-cards')
+
+    let mixer = mixitup(containerEl, {
+        classNames: {
+                block: ""
+        }
+    })
+
+})
+
+// MixItUp
